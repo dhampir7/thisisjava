4893c51da6b015ef0dadee626a59a5adcdfcd360
dhampir7 <dhampir7@naver.com>
2024-01-24 22:42:18 +0900

    24/01/24

---
+++ src/ch08/sec06/ReniteControlExample.java
@@ -0,0 +1,26 @@
+package ch08.sec06;
+
+public class ReniteControlExample {
+	
+	public static void main(String[] args) {
+		
+		RemoteControl rc;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.setVolume(5);
+		rc.turnOff();
+		
+		rc.setMute(true);
+		rc.setMute(false);
+		System.out.println("현재 볼륨: " + rc.getVolume());
+		
+		rc = new Audio();
+		rc.setVolume(7);
+		rc.setMute(true);
+		rc.setMute(false);
+		System.out.println("현재 볼륨: " + rc.getVolume());
+		
+		RemoteControl.changeBattery();
+	}
+}
