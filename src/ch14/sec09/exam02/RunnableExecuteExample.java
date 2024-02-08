e6ccddad4d45da76e623737d5e647508b7726ae7
dhampir7 <dhampir7@naver.com>
2024-02-08 23:06:52 +0900

    24/02/08

---
+++ src/ch14/sec09/exam02/RunnableExecuteExample.java
@@ -0,0 +1,18 @@
+package ch14.sec09.exam02;
+
+public class RunnableExecuteExample {
+
+	public static void main(String[] args) {
+		//1000개의 메일 생성
+		String[][] mails = new String[1000][3];
+		for(int i=0; i<mails.length; i++) {
+			mails[i][0] = "admin@my.com";
+			mails[i][1] = "admin@my.com";
+			mails[i][2] = "admin@my.com";
+		}
+		
+		//ExecutorService 생성
+		
+	}
+
+}
