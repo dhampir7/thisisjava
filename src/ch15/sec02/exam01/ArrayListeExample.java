a288e9813e94f3b168c1a2c7dd04fef5dd4b17cf
dhampir7 <dhampir7@naver.com>
2024-02-10 23:29:35 +0900

    24/02/10

--- src/ch15/sec02/exam01/ArrayListeExample.java
+++ src/ch15/sec02/exam01/ArrayListeExample.java
@@ -1,4 +1,4 @@
-package ch15;
+package ch15.sec02.exam01;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/ch15/sec02/exam01/Board.java
+++ src/ch15/sec02/exam01/Board.java
@@ -1,4 +1,4 @@
-package ch15;
+package ch15.sec02.exam01;
 
 public class Board {
 	private String subject;
